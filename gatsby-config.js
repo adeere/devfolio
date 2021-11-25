4fde5623b1941b4f34dccf75eec5a7218ca9ac34
adeere <adeere@eoas.ubc.ca>
2021-11-25 15:44:19 -0700

    changing gatsby source file

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,7 +100,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
+        path: `${__dirname}/src/pages/`,
         name: `blog`,
       },
     },
