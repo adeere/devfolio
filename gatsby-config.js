d17de535bee7a10281e6c0a787d4409bebf56723
adeere <adeere@eoas.ubc.ca>
2022-08-26 12:58:32 +0100

    editing copy

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
       },
 
     {
-    name: 'Map of Latin American Population in the United States (2019)',
+    name: 'Web-based Map of Latin American Population in the United States (2019)',
     description:
       'An interactive map of the distribution of Latin American population in the United States in 2019. Created using HTML, CSS, Javascript, and Mapbox for a final-year project looking at Migration between the US and Latin America.',
     link: 'https://adeere.github.io/adeere-web/Map3/population_2019.html',
